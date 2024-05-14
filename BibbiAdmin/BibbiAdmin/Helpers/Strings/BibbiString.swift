af839192cfbb605fe49ffa26e651ba007cba67db
Zizi_Kim <rlarjsdn3@naver.com>
2024-05-14 15:09:57 +0900

    fix: 앱 아이콘 이미지 수정 (#17)

--- BibbiAdmin/BibbiAdmin/Helpers/Strings/BibbiString.swift
+++ BibbiAdmin/BibbiAdmin/Helpers/Strings/BibbiString.swift
@@ -17,7 +17,7 @@ extension String {
 
 extension BibbiNetworkString {
     static var appKey = "7c5aaa36-570e-491f-b18a-26a1a0b72959"
-    static var xAuthToken = "eyJyZWdEYXRlIjoxNzE1MDgxOTg0NjQ4LCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsInR5cGUiOiJhY2Nlc3MifQ.eyJ1c2VySWQiOiJkcXdkcXdkcXdkcXciLCJleHAiOjE3MTUxNjgzODR9.Yo2C6Ol4iUNo8c6li41X-jn9P9Nsv6HckkYxkWwcT4Q"
+    static var xAuthToken = "eyJ0eXAiOiJKV1QiLCJyZWdEYXRlIjoxNzE1NjExNTU5ODQ1LCJ0eXBlIjoiYWNjZXNzIiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VySWQiOiLjhYjjhYLjhYfjhYjjhYIiLCJleHAiOjE3MTU2OTc5NTl9.ylUktok3of-FahKkXvvebKVJE4X4EP4HLyrOhVrA5_4"
 }
 
 extension BibbiUserInterfaceString {
